25be8c3bf9d107ddf53898086faba0c6d11b9a8a
JoseOrduno <jose.m.orduno@gmail.com>
2019-10-28 16:17:23 -0700

    creating new project

---
+++ OdeToFood/odeTToFood.Data/IRestaurantData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.core;
+
+namespace odeTToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll(); 
+    }
+}
