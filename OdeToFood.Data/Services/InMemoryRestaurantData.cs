5de58aad844c697839cf5e7d738b5f773f78516d
JoseOrduno <jose.m.orduno@gmail.com>
2019-10-29 15:53:08 -0700

    Adding Model and a displaying a list on index view

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+            new Restaurant { id = 1, Name = "Tacos", cuisine = CuisineType.Mexican },
+            new Restaurant { id = 1, Name = "Potato Rides", cuisine = CuisineType.French },
+            new Restaurant { id = 1, Name = "Pizza", cuisine = CuisineType.Italian }
+            };
+        }
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
